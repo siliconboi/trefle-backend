7732d51c3259fb87eb1564f2b67b3b8cd8087c6e
Aishwary Tripathi <aishwarytripathi2001@gmail.com>
2023-05-24 00:00:41 +0530

    route

--- index.mjs
+++ index.mjs
@@ -1,18 +1,32 @@
-import express from "express"
-import bodyParser from "body-parser"
-import cors from "cors"
-import morgan from "morgan"
-import fetch from "node-fetch"
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+import fetch from "node-fetch";
 
-const app = express()
-app.use(morgan.dev())
+const app = express();
+app.use(morgan('dev'));
+app.use(cors())
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-app.get('/token',async(req,res)=>{
-    const res = await fetch('')
-})
+app.get("/token", async () => {
+  const res = await fetch('https://trefle.io/api/auth/claim', {
+    method: "post",
+    body: {
+      "origin": "https://flower-shop-ssr.vercel.app/",
+      "token": "nPhiI1owDYQTM_cM-GzkSeO-w7_zFtZ3dRGKPwPVQ0o",
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  console.log(res)
+  const json = await res.json();
+  const token = json.token;
+  return token;
+});
 
-app.listen(PORT, ()=>{
-    console.log(`listening on https://localhost:${PORT}/`)
-})
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}/`);
+});
